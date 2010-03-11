a06cd7dfb85dd370567bd497c94b8665ac85334a
123LinkIt <123LinkIt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-11 05:23:44 +0000

    hope it now works
    git-svn-id: https://plugins.svn.wordpress.org/123linkit-affiliate-marketing-tool/trunk@216075 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/linkit_ajax.js
@@ -0,0 +1,84 @@
+function AjaxManager(pubkey, privkey){
+	this._pubkey = pubkey;
+	this._privkey = privkey;
+    this._url = getPluginDir() + "/123Linkit/simpleproxy.php";
+}
+
+AjaxManager.prototype = {
+	setKeys: function(pubkey, privkey){
+		this._pubkey  = pubkey;
+		this._privkey = privkey;
+	},
+    writeTable: function(data){
+        jQuery('div.ajax_working').fadeOut('slow');
+        var blog_id = getBlogId();
+		var contents = 0;
+		html = "Thanks for using 123LinkIt! Our goal is to automate as much as this process as possible which is why we're working on automating the embedding of the links. For now, please add them manually. This is done by right-clicking on 'Copy Link' and embedding the link into the keyword we recommend.";
+        html += "<table  class='tablesorter'><thead><tr><th>Keyword</th><th>Advertiser</th><th>Advertising Url</th><th>7 Day EPC</th><th>3 Month EPC</th><th>Copy Link</th></tr></thead><tbody>";
+        for(keyword in data.advertised){
+            for(link in data.advertised[keyword]){
+				contents = contents + 1;
+                html += "<tr><td>"+keyword+"</td><td>" + data.advertised[keyword][link].link.advertiser_name + "</td><td>"+data.advertised[keyword][link].link.link_url+"</td><td>"+data.advertised[keyword][link].link['7dayepc']+"</td><td>"+data.advertised[keyword][link].link["3monepc"]+"</td><td>";
+                html +="<a href="+getPluginDir()+"/123Linkit/links.php?bid="+blog_id+"&pid=1&aid="+data.advertised[keyword][link].link.advertiser_id+"&lid="+data.advertised[keyword][link].link.id+"&key="+getKeys()['_pubkey']+" rel=nofollow>"+keyword+"</a></td></tr>";
+            }
+        }
+		if(contents == 0)
+			html += "<tr><td colspan = '6'>We couldn't find relevant affiliate link recommendations for this post</td></tr>";
+        html +="</tbody></table>"
+        jQuery("div.result").html(html).fadeIn("slow");
+        jQuery("table").tablesorter();
+    },
+    advertise: function(data){
+        this.ajaxCall(data, '_privkey', this.writeTable);
+    },
+    showWork: function(data){
+        jQuery("div.result").fadeOut("slow");
+        jQuery("div.ajax_working").fadeIn("slow");
+    },
+	ajaxCall: function(data, type, func){
+		if(type == '_pubkey'){
+			data['_pubkey'] = this._pubkey;
+		}else{
+			data['_privkey'] = this._privkey;
+		}
+		jQuery.ajax({
+                   type: "POST",
+                   url: this._url,
+                   beforeSend: this.showWork,
+                   data: data,
+                   dataType: "json",
+                   async: false,
+                   success: func
+        });
+	},
+}
+
+function LinkitMaster(tinyMCE, pubkey, privkey){
+	this._ajaxManager = new AjaxManager(pubkey, privkey);
+	this._tinyMCE = tinyMCE;
+}
+
+LinkitMaster.prototype = {
+	getContent: function(){
+		return this._tinyMCE.activeEditor.getContent();
+	},
+	advertise: function(){
+		content = this.getContent();
+		id = getBlogId();
+        if(id){
+            links = this._ajaxManager.advertise({'url': 'getAdvertisers/'+id+'/posts/advertise.json', 'content': this.getContent()});
+        }
+        //TODO Error out and show it for me :)
+    },
+}
+
+jQuery(document).ready(function(){
+    
+    key = getKeys();
+	var linkit = new LinkitMaster(tinyMCE, key['_pubkey'], key['_privkey']);
+	
+	jQuery('.update_post').click(function(){
+		links = linkit.advertise();
+        return false;
+	});
+});
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: 123linkit
+Donate link: http://www.123linkit.com/
+Tags: comments, spam
+Requires at least: 2.0.2
+Tested up to: 2.9.2
+Stable tag: trunk
+
+123LinkIt Affiliate Plugin - Generate money easily from your blog by transforming keywords into affiliate links.
+
+== Description ==
+
+123LinkIt Affiliate Plugin - Generate money easily from your blog by transforming keywords into affiliate links. No need to apply to affiliate networks or advertisers - we do it all for you. Just pick from our list of recommendations and you're good to go! Navigate to Settings -> 123LinkIt configuration to get started.
+
+== Installation ==
+
+1. Install the plugin
+1. Create an account at [123LinkIt](http://www.123linkit.com "123LinkIt")
+1. Go to Settings -> 123LinkIt configuration and input your newly received public and private keys
+
+== Frequently Asked Questions ==
+
+= Is it free? =
+
+Yes
+
+== Screenshots ==
+
+No Screenshots
+
+== Changelog ==
+
+= 0.0.1 =
+* Alpha release
+
+== Upgrade Notice ==
+
+No Upgrade Notices
