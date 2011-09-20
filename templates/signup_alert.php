fff49b7bafa4647d5d0f35218297e4df2286995e
strangerstudios <strangerstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-20 14:44:46 +0000

    1.3.1 pagination bug fix and new user streamlining
    git-svn-id: https://plugins.svn.wordpress.org/123linkit-affiliate-marketing-tool/trunk@440835 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/signup_alert.php
+++ templates/signup_alert.php
@@ -1,7 +1,7 @@
 <div id="linkitnotification" class="error">
 <h2>Thanks for using 123LinkIt!</h2>
 
-<p><b>To make this message go away:</b> You must configure your <a href="admin.php?page=linkit_options">settings</a>
+<p><b>To make this message go away:</b> You must <a href="admin.php?page=linkit_plugin&step=signup">create an account</a> and configure your <a href="admin.php?page=linkit_options">settings</a>
 		and select your blog category before using the 123Linkit plugin!</p>
 
 <p><b>Note: You only need to synchronize your posts once.</b> Our plugin will 
